19d927428cd06fc2d8a0f2ecbd2ea196b8b7b62c
Kashish <kashishpatel7014@gmail.com>
2023-05-30 17:29:51 -0400

    created and tested on 30th may

---
+++ systemidentification.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#to check ip address
+ip add
+
+#to check default ip gateway
+ip route
+
+#to check hostname 
+hostname
+
+#to check whether the computer is online or offline
+systemctl status
